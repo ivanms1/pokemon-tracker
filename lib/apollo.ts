55fd1cfc332a774fc46355d397ba0374e30b4fcf
Ivan Saldano <ivanms1@gmail.com>
2021-10-01 11:24:07 +0900

    change vercel url variable

--- lib/apollo.ts
+++ lib/apollo.ts
@@ -3,7 +3,7 @@ import { ApolloClient, InMemoryCache } from "@apollo/client";
 export const client = new ApolloClient({
   uri:
     process.env.NODE_ENV === "production"
-      ? `https://${process.env.VERCEL_URL}/api/graphql`
+      ? `https://${process.env.NEXT_PUBLIC_VERCEL_URL}/api/graphql`
       : "http://localhost:3000/api/graphql",
   cache: new InMemoryCache(),
 });
